8bb2b6d1f80630a12b06f758ec1d8c9f8ad24bf8
Maxnightik <xiaomimax4n@gmail.com>
2024-03-05 22:59:05 +0200

    daytwo

---
+++ js/service.js
@@ -0,0 +1,18 @@
+const API_KEY = "49a4a60ab3a5c35e818e228a86751603";
+const BASE_URL = "https://api.themoviedb.org/3/";
+const LANGUAGE = "&language=uk-UA";
+
+const getData = (url) =>
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw `Ой, щось пішло не так, помилка ${response.status}`;
+    })
+    .catch((err) => console.error(err));
+
+export const getTriends = async (type = "all", period = "day", page = 6) => {
+  const url = `${BASE_URL}trending/${type}/${period}?api_key=${API_KEY}${LANGUAGE}&page=${page}`;
+  return await getData(url);
+};
